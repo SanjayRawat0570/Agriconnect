cc931e2e5f5930849936a0bbbbfe24b53c52423d
Rahul Rawat <rahulrawat4060@gmail.com>
2025-04-26 15:33:57 +0530

    add meta Data

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,20 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'AgriConnect',
+  description: 'Connecting Farmers and Consumers',
+  generator: 'Next.js',
+  applicationName: 'AgriConnect',
+  referrer: 'origin-when-cross-origin',
+  keywords: ['AgriConnect', 'Farmers', 'Consumers', 'Agriculture'],
+  authors: [{ name: 'AgriConnect Team'}],
+  colorScheme: 'light dark',
+  creator: 'AgriConnect Team',
+  publisher: 'AgriConnect Team',
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: '#ffffff' },
+    { media: '(prefers-color-scheme: dark)', color: '#000000' },
+  ]
 }
 
 export default function RootLayout({
